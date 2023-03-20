8c31cfb1cc275039a178f1c261bddd0a1beee36e
LucasAlencar <lucas.linard@outlook.com>
2023-03-20 12:37:44 -0300

    refactor: css helper for theme on Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,31 +1,31 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TouchableOpacity } from "react-native";
 
-export type ButtonTypeStyleProps =  "PRIMARY" | "SECONDARY";
+export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
 
 type Props = {
-    type: ButtonTypeStyleProps;
-}
-
-
-
+  type: ButtonTypeStyleProps;
+};
 
 export const Container = styled(TouchableOpacity)<Props>`
-    flex: 1;
+  flex: 1;
 
-    min-height: 56px;
-    max-height: 56px;
+  min-height: 56px;
+  max-height: 56px;
 
-    background-color: ${({ theme, type }) => type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};   
+  background-color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
 
-    border-radius: 6px;
+  border-radius: 6px;
 
-    justify-content: center;
-    align-items: center;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Title = styled.Text`
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-    font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-    color: ${({ theme }) => theme.COLORS.WHITE};
-`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.WHITE};
+  `}
+`;
