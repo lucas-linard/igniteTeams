2cb6f16692aa7017a92516ee2c9aed6b64af56ec
LucasAlencar <lucas.linard@outlook.com>
2023-03-20 12:39:00 -0300

    feat: New group screen

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+import { Container, Content, Icon } from "./styles";
+
+export default function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton={true} />
+      <Content>
+        <Icon/>
+        <Highlight
+        title="Nova Turma"
+        subtitle="Cira a turma para adicionar pessosas"
+        />
+        <Input
+        placeholder="Nome da turma"
+        />
+        <Button
+        title="Criar"
+        style={{ marginTop: 20 }}
+        />
+      </Content>
+    </Container>
+  );
+}
