0544a03b1254d545fbd8d1d2e4745fb3c3e59ba9
LucasAlencar <lucas.linard@outlook.com>
2023-03-20 18:34:05 -0300

    fix: font-size px

--- src/components/ListEmpty/styles.ts
+++ src/components/ListEmpty/styles.ts
@@ -10,7 +10,7 @@ export const Message = styled.Text`
   text-align: center;
 
   ${({ theme }) => css`
-  font-size: ${theme.FONT_SIZE.SM};
+  font-size: ${theme.FONT_SIZE.SM}px;
   font-family: ${theme.FONT_FAMILY.REGULAR};
   color: ${theme.COLORS.GRAY_300};
   `}
