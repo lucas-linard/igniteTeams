9e2e30e716ce40980ca6e581fa4b562a039f1ec7
LucasAlencar <lucas.linard@outlook.com>
2023-03-20 11:22:23 -0300

    feat: Flatlist ListEmptyComponent

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,8 +4,9 @@ import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 export default function Group() {
-  const [groups, setGroups] = useState<string[]>(['Galera do vava']);
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
@@ -18,6 +19,8 @@ export default function Group() {
           title={item} 
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={<ListEmpty message="Que tal cadastrar a primeira turma?" />} 
       />
     </Container>
   );
