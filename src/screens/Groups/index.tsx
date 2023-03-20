fda4400630515a819f5736b5c478cc7bfa076103
LucasAlencar <lucas.linard@outlook.com>
2023-03-20 11:00:20 -0300

    feat: Listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 export default function Group() {
+  const [groups, setGroups] = useState<string[]>(['Galera do vava']);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-
-      <GroupCard title="Galera do vava" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+        <GroupCard
+          title={item} 
+          />
+        )}
+      />
     </Container>
   );
 }
