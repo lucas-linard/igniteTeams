a3acf7d02b562206cccab093ae2ab7585ef55afc
LucasAlencar <lucas.linard@outlook.com>
2023-03-21 16:12:44 -0300

    feat: Header handleGoBack

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import { Container, Logo, BackButton, BackIcon } from "./style";
 import logoImg from "@assets/logo.png";
 
+import { useNavigation } from "@react-navigation/native";
 type Props = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
