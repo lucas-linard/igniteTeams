876453377f1aa6aaefdc5d28dd1fa58da5516f59
LucasAlencar <lucas.linard@outlook.com>
2023-03-21 16:09:09 -0300

    feat: Navigation

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { Loading } from "@components/Loading";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
-import Groups from "@screens/Groups";
+import { Routes } from "./src/routes/index";
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
