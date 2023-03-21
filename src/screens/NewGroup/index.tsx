cce1cbd164efddb246a9765d1b3dc2bbb008fefc
LucasAlencar <lucas.linard@outlook.com>
2023-03-21 16:32:31 -0300

    feat: navigate to Players

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -5,21 +6,25 @@ import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export default function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("Players", { group: "Turma 1" });
+  }
   return (
     <Container>
       <Header showBackButton={true} />
       <Content>
-        <Icon/>
+        <Icon />
         <Highlight
           title="Nova Turma"
-        subtitle="Cira a turma para adicionar pessosas"
+          subtitle="Criar a turma para adicionar pessoas"
         />
-        <Input
-        placeholder="Nome da turma"
-        />
+        <Input placeholder="Nome da turma" />
         <Button
-        title="Criar"
-        style={{ marginTop: 20 }}
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNewGroup}
         />
       </Content>
     </Container>
